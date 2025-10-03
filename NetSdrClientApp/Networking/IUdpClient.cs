6e5e0b5ab1644645bbe0126cbf3f3b4fad58159c
akulkaaaaaaaaaaaaa <125921776+akulkaaaaaaaaaaaaa@users.noreply.github.com>
2025-10-03 10:55:49 +0300

    Move 'IUdpClient' into a named namespace.

--- NetSdrClientApp/Networking/IUdpClient.cs
+++ NetSdrClientApp/Networking/IUdpClient.cs
@@ -1,10 +1,19 @@
-﻿
-public interface IUdpClient
-{
-    event EventHandler<byte[]>? MessageReceived;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-    Task StartListeningAsync();
-
-    void StopListening();
-    void Exit();
+namespace NetSdrClientApp.Networking
+{﻿
+    public interface IUdpClient
+    {
+        event EventHandler<byte[]>? MessageReceived;
+    
+        Task StartListeningAsync();
+    
+        void StopListening();
+        void Exit();
+    }
 }
